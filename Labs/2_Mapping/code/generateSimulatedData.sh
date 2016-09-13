09754ce638c411f00f847acde86fed554fc7e842
Nicholas D. Socci <soccin@mskcc.org>
2016-09-14 00:10:55 +0200

    Added genome path

--- Labs/2_Mapping/code/generateSimulatedData.sh
+++ Labs/2_Mapping/code/generateSimulatedData.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+ROOT45=/share/data/compgen2016/day45_Intro2Seq_VarCalling
+GENOME=$ROOT45/genomes/H.Sapiens/b37_hl/b37_hl.fa
+
 wgsim -N 100000 -1 50 -2 50 $GENOME \
 	b37Test2_DEF_1_SE.fastq /dev/null
 
